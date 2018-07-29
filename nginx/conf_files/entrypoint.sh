b7ca6f8d0c71ece0b7f02d25b8d2db92b0e97fa2
Roozbeh Shafiee <roozbeh.shafiee@gmail.com>
2018-07-29 17:01:21 +0200

    modifying nginx dockerfile and its entrypoint.sh

--- nginx/conf_files/entrypoint.sh
+++ nginx/conf_files/entrypoint.sh
@@ -6,19 +6,16 @@ set -e
 FQDN=${FQDN:-}
 SUPPORT_SSL=${SUPPORT_SSL:-}
 
-initialize() {
-      rm /etc/nginx/sites-available/default
-      rm /etc/nginx/sites-enabled/default
-      echo "<?php phpinfo(); ?>" > /data/index.php
-}
-
 support_ssl() {
   if [ $SUPPORT_SSL = "yes" ]; then
       sed -i "s|FQDN|${FQDN}|g" /etc/nginx/sites-available/443-default
       ln -s /etc/nginx/sites-available/443-default /etc/nginx/sites-enabled/443-default
+      chmod 600 /etc/nginx/ssl/*
+      echo "<?php phpinfo(); ?>" > /data/index.php
   else
       sed -i "s|FQDN|${FQDN}|g" /etc/nginx/sites-available/80-default
       ln -s /etc/nginx/sites-available/80-default /etc/nginx/sites-enabled/80-default
+      echo "<?php phpinfo(); ?>" > /data/index.php
   fi
 }
 
@@ -26,6 +23,5 @@ run_nginx() {
       nginx -g 'daemon off;'
 }
 
-initialize
 support_ssl
 run_nginx
