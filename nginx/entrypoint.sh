77b6e8f21d6664a1b9373c014f6a76364ed25b09
Roozbeh Shafiee <roozbeh.shafiee@gmail.com>
2019-02-09 04:23:00 +0100

    adding letsencrypt to nginx

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -3,16 +3,24 @@
 set -m
 set -e
 
-FQDN=${FQDN:-}
+DOMAIN_NAME=${DOMAIN_NAME:-}
+EMAIL_ADDRESS=${EMAIL_ADDRESS:-}
 
-support_ssl() {
-      sed -i "s|FQDN|${FQDN}|g" /etc/nginx/conf.d/default.conf
-      chmod 600 /etc/nginx/ssl/*
-}
+nginx -g 'daemon off;'
 
-run_nginx() {
-      nginx -g 'daemon off;'
-}
+if [[ -d "/etc/letsencrypt/live/${DOMAIN_NAME}" ]]; then
+        certbot renew --quiet
+else
+        if ! [[ -d "/etc/letsencrypt/live/${DOMAIN_NAME}" ]]; then
+                certbot --nginx -m ${EMAIL_ADDRESS} --agree-tos --no-eff-email --redirect --expand -d ${DOMAIN_NAME},www.${DOMAIN_NAME}
+        fi
+        if ! [[ -f "/etc/ssl/certs/dhparam.pem" ]]; then
+                openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+        fi
+fi
 
-support_ssl
-run_nginx
+sed -i "s|DOMAIN_NAME|${DOMAIN_NAME}|g" /etc/nginx/conf.d/default
+rm /etc/nginx/conf.d/default.conf
+mv /etc/nginx/conf.d/default /etc/nginx/conf.d/default.conf
+
+nginx -s stop && nginx -g 'daemon off;'
